fff79a453e54f2c67deb89569893a8d080e4c200
juan <jtula@mktmarketingdigital.com>
2022-01-14 15:57:16 -0300

    various fixes & sitemap

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Spatie\Sitemap\SitemapServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
