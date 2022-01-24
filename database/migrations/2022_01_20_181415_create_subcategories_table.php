0e304bd8f0a3b082283fcf07d7724ebf447c3598
MatiasOrcajo <matiasorcajo3@gmail.com>
2022-01-24 16:32:57 -0300

    fix a un error en la migracion que hacia que rompa la vista productos del admin

--- database/migrations/2022_01_20_181415_create_subcategories_table.php
+++ database/migrations/2022_01_20_181415_create_subcategories_table.php
@@ -16,7 +16,8 @@ class CreateSubcategoriesTable extends Migration
         Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
             $table->text('title');
-            $table->unsignedInteger('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
