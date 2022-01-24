28bdfa4d8578d8c63a2029f1e42344fe8d76453b
MatiasOrcajo <matiasorcajo3@gmail.com>
2022-01-24 11:59:35 -0300

    subo favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
     @endif
     <title>{{env('APP_NAME')}}</title>
 
+    <link rel="icon" href="{{asset('img/tarifario-icon.png')}}" type="image/x-icon">
+
     <!-- CDNs de Font-Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
